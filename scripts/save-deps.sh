4b4496e983a2d612149d3182a8fdffcd70de37fc
aby913 <aby913@163.com>
2024-08-21 21:19:35 +0800

    no message

--- scripts/save-deps.sh
+++ scripts/save-deps.sh
@@ -10,11 +10,15 @@ fileprefix="deps"
 name=$(md5sum dependencies.mf |awk '{print $1}')
 echo "READY to find this deps ${fileprefix}-${name}.tar.gz"
 
-curl -fsSLI https://dc3p1870nn3cj.cloudfront.net/${path}${fileprefix}-${name}.tar.gz > /dev/null
+# curl -fsSLI https://dc3p1870nn3cj.cloudfront.net/${path}${fileprefix}-${name}.tar.gz > /dev/null
+aws s3 ls s3://zhangliang-s3-test/test/${path}${fileprefix}-$name.tar.gz
 if [ $? -eq 0 ]; then
-    curl -fsSL https://dc3p1870nn3cj.cloudfront.net/${path}${fileprefix}-${name}.tar.gz -o ${fileprefix}-${name}.tar.gz
-    tar -zxvf ${fileprefix}-${name}.tar.gz
-    rm -rf ${fileprefix}-${name}.tar.gz
+    echo "---a1---"
+    # curl -fsSL https://dc3p1870nn3cj.cloudfront.net/${path}${fileprefix}-${name}.tar.gz -o ${fileprefix}-${name}.tar.gz
+
+    # tar -zxvf ${fileprefix}-${name}.tar.gz
+    # rm -rf ${fileprefix}-${name}.tar.gz
 else
+    echo "---a2---"
     bash ${BASE_DIR}/download-deps.sh $PLATFORM
 fi
