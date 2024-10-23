39970d42347f5cda24aa332e28de38e8cd0e4875
aby913 <aby913@163.com>
2024-10-23 15:41:55 +0800

    no message

--- scripts/build-wsl-image.sh
+++ scripts/build-wsl-image.sh
@@ -8,9 +8,7 @@ USER=ubuntu
 cat > ./Dockerfile.v${VERSION} << _END
 FROM ubuntu:22.04
 
-# ARG USER
-
-# RUN apt-get update -y && apt-get -y install iproute2 curl sudo software-properties-common pciutils openssh-client iputils-ping vim
+RUN apt-get update -y && apt-get -y install iproute2 curl sudo software-properties-common pciutils openssh-client iputils-ping vim
 
 RUN /bin/bash -c 'addgroup ${USER}; useradd -m -s /bin/bash -g ${USER} ${USER}; echo "${USER}:1" | chpasswd'
 
@@ -35,25 +33,7 @@ _END
 
 name="install-wizard-wsl-image-v${VERSION}"
 checksum="$name.checksum.txt"
-
-echo "---1--- [${DIST_PATH}] [${BASE_DIR}]"
-pwd
-echo "---2---"
-ls
-echo "---3---"
-ls ${BASE_DIR}/
-echo "---4---"
-ls ${DIST_PATH}/
-echo "---5---"
-
-cat ./Dockerfile.v${VERSION}
-echo "---6---"
-cat ./wsl.conf
-echo "---7---"
-
 cp ${DIST_PATH}/install.sh ./
-echo "---8---"
-cat ./install.sh
 
 # curl -fsSLI https://dc3p1870nn3cj.cloudfront.net/$name.tar.gz > /dev/null
 aws s3 ls s3://zhangliang-s3-test/test2/$name.tar.gz > /dev/null
@@ -63,7 +43,6 @@ if [ $? -ne 0 ]; then
     # --build-arg USER=ubuntu 
     docker build -f ./Dockerfile.v${VERSION} -t install-wizard:v${VERSION} .
     cid=$(docker run -it --name terminus-v${VERSION} -d install-wizard:v${VERSION})
-    # echo "---4--- ${cid}"
     docker export -o ./${name}.tar.gz ${cid}
     md5sum ./${name}.tar.gz > ./${checksum}
 
@@ -72,8 +51,3 @@ if [ $? -ne 0 ]; then
     echo "upload $name completed"
     set +e
 fi
-
-
-echo "---9---"
-ls
-echo "---10---"
